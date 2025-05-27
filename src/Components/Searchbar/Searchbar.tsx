45caf335d2bf4e7fe1ff668a7997fd489f217885
Sirbi <osilvio123@gmail.com>
2025-05-27 18:57:07 -0300

    style/ ajsutes temporários no financeiro e no  barra de pesquisa

--- src/Components/Searchbar/Searchbar.tsx
+++ src/Components/Searchbar/Searchbar.tsx
@@ -18,16 +18,16 @@ export default function Searchbar() {
             placeholder="Pesquise seu destino"
             keyboardType="default"
             maxLength={99}
-            style={tw`w-66 h-12 border-2 rounded-lg`}
+            style={tw`w-66 h-12 border-2 rounded-lg border-[#313135]`}
           ></TextInput>
         </View>
-        <TouchableOpacity style={tw`w-12 h-12 bg-[#676150] rounded-lg items-center justify-center`}>
-          <MaterialCommunityIcons name={"magnify"} size={40} color={"#F1D014"} />
+        <TouchableOpacity style={tw`w-12 h-12 bg-[#313135] rounded-lg items-center justify-center`}>
+          <MaterialCommunityIcons name={"magnify"} size={40} color={"#6E92C0"} />
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => setFiltroVisible(true)}
-        style={tw`w-12 h-12 bg-[#676150] rounded-lg items-center justify-center`}>
-          <MaterialCommunityIcons name={"filter"} size={40} color={"#F1D014"} />
+        style={tw`w-12 h-12 bg-[#313135] rounded-lg items-center justify-center`}>
+          <MaterialCommunityIcons name={"filter"} size={40} color={"#6E92C0"} />
         </TouchableOpacity>
       </View>
 
