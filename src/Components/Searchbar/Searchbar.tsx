9be9d6b9a195a008c5a2cf6767b4d91ea144684a
EduSG <eduardo.gomes.072020@gmail.com>
2025-05-27 23:45:38 -0300

    feat: reload ao buscar

--- src/Components/Searchbar/Searchbar.tsx
+++ src/Components/Searchbar/Searchbar.tsx
@@ -1,15 +1,23 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, TextInput } from "react-native";
 import tw from "twrnc";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import ModalFiltro from "../Modals/ModalFiltro/Index";
 
+type Props = {
+  visible: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  onSave: (form: any) => void;
+};
 
-
-export default function Searchbar() {
-
+export default function Searchbar({ visible, onOpen, onClose, onSave }: Props) {
   const [filtroVisible, setFiltroVisible] = useState(false);
 
+  const handleSave = (formData: any) => {
+    console.log("Filtro aplicado:", formData);
+    setFiltroVisible(false);
+  };
   return (
     <View style={tw`mx-5`}>
       <View style={tw`flex-row justify-center gap-x-2 mt-2`}>
@@ -21,21 +29,29 @@ export default function Searchbar() {
             style={tw`w-66 h-12 border-2 rounded-lg`}
           ></TextInput>
         </View>
-        <TouchableOpacity style={tw`w-12 h-12 bg-[#676150] rounded-lg items-center justify-center`}>
-          <MaterialCommunityIcons name={"magnify"} size={40} color={"#F1D014"} />
+        <TouchableOpacity
+          style={tw`w-12 h-12 bg-[#676150] rounded-lg items-center justify-center`}
+        >
+          <MaterialCommunityIcons
+            name={"magnify"}
+            size={40}
+            color={"#F1D014"}
+          />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => setFiltroVisible(true)}
-        style={tw`w-12 h-12 bg-[#676150] rounded-lg items-center justify-center`}>
+        <TouchableOpacity
+          onPress={onOpen}
+          style={tw`w-12 h-12 bg-[#676150] rounded-lg items-center justify-center`}
+        >
           <MaterialCommunityIcons name={"filter"} size={40} color={"#F1D014"} />
         </TouchableOpacity>
       </View>
 
-    <ModalFiltro
-            visible={filtroVisible}
-            onClose={() => setFiltroVisible(false)}
-          />
-
+      <ModalFiltro
+        visible={visible}                           
+        onClose={onClose}
+        onSave={onSave}
+      />
     </View>
   );
 }
